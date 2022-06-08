a993e9208f71a2dcd3d06b2b290a77ce21330780
CMiller <mlr.chase40@gmail.com>
2022-06-08 16:23:59 -0500

    add Post module

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import and congrigate all models
 const User = require('./User');
+const Post = require('./Post');
 
 
 // export all models for project use
-module.exports = { User };
+module.exports = { User, Post };
