9423c0b1605f937d811f53340787b7952622cc4a
CMiller <mlr.chase40@gmail.com>
2022-06-08 21:54:18 -0500

    completed CRUD routes for Posts

--- models/index.js
+++ models/index.js
@@ -2,6 +2,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create assoctiations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 // export all models for project use
 module.exports = { User, Post };
