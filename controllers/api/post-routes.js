ed13208057a862004adf00471bd4f9e62213fe5e
CMiller <mlr.chase40@gmail.com>
2022-06-20 19:51:37 -0500

    add single post page; add login authenication for seeing comments and voting

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -110,13 +110,21 @@ router.post('/', (req, res) => {
 
 // UPDATE /api/posts/upvote - add vote to post
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructed properties on req.body
+
+    // custom static method created in models/Post.js
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // UPDATE /api/posts/? - update post via id
